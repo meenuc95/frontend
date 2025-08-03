3b5a146ff846329dbbf26b2093eeae5e10d84ff2
meenuc95 <meenuc95@gmail.com>
2025-08-03 18:32:57 +0530

    change

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -10,7 +10,7 @@ class ListEmployee extends React.Component {
 	}
 	
 	loadData() {
-        const employeeAPIDomain = process.env.REACT_APP_EMPLOYEE_API_DOMAIN || 'http://13.200.237.169:8080/api/v1';
+        const employeeAPIDomain = process.env.REACT_APP_EMPLOYEE_API_DOMAIN || 'http://54.169.120.250:8080/api/v1';
         const endpoint = `${employeeAPIDomain}/employee/search/all`;
 		fetch(endpoint)
 			.then(response => response.json())
